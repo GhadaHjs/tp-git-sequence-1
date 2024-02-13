20481bc6a270b3eb661db2e4e809e92137e7c522
Ghada <ghadahjs1@gmail.com>
2024-02-13 14:51:06 +0100

    bienvenue

---
+++ bienvenue.cpp
@@ -0,0 +1,12 @@
+// Affiche un message de bienvenue
+#include <iostream>
+int main()
+{
+std::cout << "Bienvenue le monde !" << std::endl;
+return 0;
+}// TODO Indiquer ce que fait le programme
+int main()
+{
+// TODO Afficher un message de bienvenue
+return 0;
+}
